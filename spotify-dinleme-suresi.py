aadd2757a5ebe5b4cbe990b1027ac25f5226e49b
arifgore <arifgore@hotmail.com>
2022-03-30 14:33:25 +0300

    Yıl hesabından ay hesabına geçildi

--- spotify-dinleme-suresi.py
+++ spotify-dinleme-suresi.py
@@ -1,4 +1,5 @@
 import json
+months = ["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"]
 
 jsonList = []
 i=0
@@ -12,15 +13,19 @@ while(True):
     except:
         break
 
-msEachYear = {}
-
+msEachMonth = {}
 for i in(jsonList):
-    if( i["endTime"][:4]  in  msEachYear.keys()):
-        msEachYear[i["endTime"][:4]] += i["msPlayed"]
+    if( i["endTime"][:4]  in  msEachMonth.keys()):
+        if(months[int(i["endTime"][5:7])-1] in msEachMonth[i["endTime"][:4]].keys()):
+            msEachMonth[i["endTime"][:4]][months[int(i["endTime"][5:7])-1]] += i["msPlayed"]
+        else:
+            msEachMonth[i["endTime"][:4]][months[int(i["endTime"][5:7])-1]] = i["msPlayed"]
     else:
-        msEachYear[i["endTime"][:4]] = i["msPlayed"]
+        msEachMonth[i["endTime"][:4]] = {months[int(i["endTime"][5:7])-1]:i["msPlayed"]}
 
-for j in(msEachYear.keys()):
-    print(f"\n{j} yılında {int(msEachYear[j]/60000)} dakika",end="")
+
+for j in(msEachMonth.keys()):
+    for k in(msEachMonth[j].keys()):
+        print(f"\n{j} yılının {k}{(8-len(k))*' '} ayında {int(msEachMonth[j][k]/60000)}{(6-len(str(int(msEachMonth[j][k]/60000))))*' '} dakika",end="")
 
 print(" müzik dinlediniz.")    
