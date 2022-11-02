f5bce4a636ba0a1059ad81c2c7ce9435b610ae08
arifgore <arifgore@hotmail.com>
2022-11-02 19:25:35 +0300

    Small fixes

--- spotify_statistics.py
+++ spotify_statistics.py
@@ -38,18 +38,15 @@ for i in(jsonList):
         msEachMonth[i["endTime"][:4]] = {months[int(i["endTime"][5:7])-1]:i["msPlayed"]}
 
 artistCount = sorted(artistCount.items(),key= lambda x: x[1][1], reverse= True)
-print(artistCount[0])
 statsFile = open("listening_statistics.txt", "wt")
 
 for i in artistCount:
     if(int(i[1][1]/60000) > 0):
-        statsFile.write(f"{i[0]}{(70-len(i[0])) * ' '} {i[1][0]}{(6-len(str(i[1][0])))*' '} kez {int(i[1][1]/60000)}{(6 - len(str(int(i[1][1]/60000)))) * ' '} dakika\n")
-        statsFile.write("\n")
+        statsFile.write(f"{i[0]}{(70-len(i[0])) * ' '} {i[1][0]}{(6-len(str(i[1][0])))*' '} kez {int(i[1][1]/60000)}{(6 - len(str(int(i[1][1]/60000)))) * ' '} dakika\n\n")
         songCount = sorted(i[1][2].items(), key= lambda x: x[1][1], reverse=True)
         for j in songCount:
             if(int(j[1][1]/60000) > 0):
-                statsFile.write(f"{5 * ' '}{j[0][:65]}{(70-len(j[0][:65])) * ' '} {j[1][0]}{(6-len(str(j[1][0])))*' '} kez {int(j[1][1]/60000)}{(6 - len(str(int(j[1][1]/60000)))) * ' '} dakika\n")
-    statsFile.write("\n\n")    
+                statsFile.write(f"{5 * ' '}{j[0][:60]}{(65-len(j[0][:60])) * ' '} {j[1][0]}{(6-len(str(j[1][0])))*' '} kez {int(j[1][1]/60000)}{(6 - len(str(int(j[1][1]/60000)))) * ' '} dakika\n\n")
 
 statsFile.close()
 
